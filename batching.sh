78908fa0e2f70ea5770f193f9146ad51466f91fe
globalworming <andreas.worm@headissue.com>
2021-01-06 12:16:59 +0100

    remove unused parameter

--- batching.sh
+++ batching.sh
@@ -1,3 +1,3 @@
- mvn verify -Dparallel.tests=2 -Dserenity.batch.count=3 -Dserenity.batch.number=1 | tee 1.txt &
- mvn verify -Dparallel.tests=2 -Dserenity.batch.count=3 -Dserenity.batch.number=2 | tee 2.txt &
- mvn verify -Dparallel.tests=2 -Dserenity.batch.count=3 -Dserenity.batch.number=3 | tee 3.txt &
+ mvn verify -Dserenity.batch.count=3 -Dserenity.batch.number=1 | tee 1.txt &
+ mvn verify -Dserenity.batch.count=3 -Dserenity.batch.number=2 | tee 2.txt &
+ mvn verify -Dserenity.batch.count=3 -Dserenity.batch.number=3 | tee 3.txt &
